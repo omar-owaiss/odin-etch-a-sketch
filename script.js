9779da114784337f51ee72a2c520e207a54839f8
omar owaiss <omarowaiss15@gmail.com>
2026-01-02 18:33:14 +0200

    Add the hovering effect for the grid pixels

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const container = document.querySelector(".container")
 
 for (let i = 0; i < 256; i++) {
    let grid = document.createElement("div")
-//    let para = document.createElement("p")
-//    para.textContent = "lol"
-//    grid.appendChild(para)
    grid.classList.add("grid")
-//    grid.style.backgroundColor = "black"
-//    grid.style.width = "50px"
     container.appendChild(grid)
 }
 
-// container.style.backgroundColor = "black"
-// container.style.width = "50px"
+
+const boxes = document.querySelectorAll(".grid")
+
+boxes.forEach(box => {
+    box.addEventListener("mouseenter", () => {
+        box.classList.add("touched")
+    })
+})
+
