936b0b949d42be5afb60dde304e7a097eae7e1b1
omar owaiss <omarowaiss15@gmail.com>
2026-01-03 19:51:25 +0200

    Change js code to allow dynamic sizing of squares inside the grid

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const container = document.querySelector(".container")
 const btnReset = document.querySelector(".reset-button")
 
-for (let i = 0; i < 256; i++) {
-   let grid = document.createElement("div")
-   grid.classList.add("grid")
-    container.appendChild(grid)
-}
+function createGrid(num) {
+    container.innerHTML = ""
+    for (let i = 0; i < num * num; i++) {
+      let grid = document.createElement("div")
+      grid.classList.add("grid")
+     const squareSize = 350 / num
+      grid.style.width = `${squareSize}px`
+      grid.style.height = `${squareSize}px`
 
+      container.appendChild(grid)
+      const boxes = document.querySelectorAll(".grid")
 
-const boxes = document.querySelectorAll(".grid")
-
-boxes.forEach(box => {
-    box.addEventListener("mouseenter", () => {
-        box.classList.add("touched")
-    })
-})
-
-btnReset.addEventListener("click", () => {
     boxes.forEach(box => {
-        box.classList.remove("touched")
+       box.addEventListener("mouseenter", () => {
+           box.classList.add("touched")
+       })
+   })
+
+    btnReset.addEventListener("click", () => {
+        boxes.forEach(box => {
+            box.classList.remove("touched")
+        })
     })
-})
+}}
+
+
+createGrid(16)
