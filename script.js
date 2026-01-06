e1e221c5f517a6dd385ce97842234ad97c6c607e
omar owaiss <omarowaiss15@gmail.com>
2026-01-06 12:43:05 +0200

    Change event listners for coloring to improve performance

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".container")
 const btnReset = document.querySelector(".reset-button")
+const btnSize = document.querySelector(".size-button")
+
 
 function createGrid(num) {
     container.innerHTML = ""
@@ -11,22 +13,28 @@ function createGrid(num) {
       grid.style.height = `${squareSize}px`
 
       container.appendChild(grid)
-      const boxes = document.querySelectorAll(".grid")
+      
+}}
+const boxes = document.querySelectorAll(".grid")
 
-    boxes.forEach(box => {
-       box.addEventListener("mouseenter", () => {
-           box.classList.add("touched")
-       })
-   })
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("grid")) {
+        e.target.classList.add("touched")
+    }
+})
+// boxes.forEach(box => {
+//        box.addEventListener("mouseenter", () => {
+//            box.classList.add("touched")
+//        })
+//    })
+
 
     btnReset.addEventListener("click", () => {
         boxes.forEach(box => {
             box.classList.remove("touched")
         })
     })
-}}
 
-const btnSize = document.querySelector(".size-button")
 
 btnSize.addEventListener("click", () => {
     let newSize = prompt("Please pick a number between 1-100 for the new grid size: ", 16)
@@ -38,6 +46,64 @@ btnSize.addEventListener("click", () => {
         alert("please pick a number between 1-100")
     }
 })
-
-
 createGrid(16)
+
+
+
+
+
+
+
+
+
+// const container = document.querySelector('.container');
+// const resetButton = document.querySelector('.reset-button');
+// const sizeButton = document.querySelector('.size-button');
+
+// function createGrid(size) {
+//     // Clear the grid
+//     container.innerHTML = '';
+    
+//     // Use DocumentFragment for performance
+//     const fragment = document.createDocumentFragment();
+//     const squareSize = 350 / size;
+    
+//     for (let i = 0; i < size * size; i++) {
+//         const gridSquare = document.createElement("div");
+//         gridSquare.classList.add("grid");
+//         gridSquare.style.width = `${squareSize}px`;
+//         gridSquare.style.height = `${squareSize}px`;
+        
+//         fragment.appendChild(gridSquare);
+//     }
+    
+//     container.appendChild(fragment);
+// }
+
+// // Event delegation - ONE listener for all squares
+// container.addEventListener('mouseover', (e) => {
+//     if (e.target.classList.contains('grid')) {
+//         e.target.classList.add('touched');
+//     }
+// });
+
+// // Reset button
+// resetButton.addEventListener('click', () => {
+//     const squares = document.querySelectorAll('.grid');
+//     squares.forEach(square => square.classList.remove('touched'));
+// });
+
+// // Size change button
+// sizeButton.addEventListener('click', () => {
+//     let newSize = prompt("Enter grid size (max 64):");
+//     newSize = parseInt(newSize);
+    
+//     if (newSize && newSize > 0 && newSize <= 64) {
+//         createGrid(newSize);
+//     } else {
+//         alert("Please enter a number between 1 and 64");
+//     }
+// });
+
+// // Initial grid
+// createGrid(16);
